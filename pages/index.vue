74bdd2ae60fdb90080ef7f0d8c3bfaa0eba6b573
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-01-15 00:18:36 +0530

    Add Summary section and period selection Use Nuxt UI SelectMenu Component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {transactionViewOptions} from '../constants'
+
+const selectedView = ref(transactionViewOptions[1]);
+</script>
 
 <template>
-    <div>Index Page!</div>
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu :options="transactionViewOptions" v-model="selectedView" />
+    </div>
+  </section>
 </template>
