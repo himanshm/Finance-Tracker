4e336230d4861b4cc2163bc7d0aea68514ce33bb
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-01-14 23:38:58 +0530

    Initial commit Add new nuxt project Add a default layout for the app Add fonts and background colors

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+// Use Google fonts
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+      crossorigin: '',
+    },
+  ],
+});
+</script>
+
+<template>
+  <div class="container mx-auto max-w-4xl">
+    <main class="my-10">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style>
+/*  Set the font family for entire site */
+
+body {
+  font-family: 'Inter', sans-serif;
+}
+
+/* Set the background color for the entire site */
+body {
+  @apply bg-white dark:bg-gray-900;
+}
+</style>
