8d1e256af9fcafbbce98441e701993ba8b66c4d3
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-01-19 20:45:49 +0530

    Add a form to add user on profile page Changes to be committed: 	modified:   pages/settings/profile.vue

--- pages/settings/profile.vue
+++ pages/settings/profile.vue
@@ -1,3 +1,47 @@
 <template>
-    Profile!
+  <UFOrm
+    :state="state"
+    :schema="schema">
+    <UFormGroup
+      class="mb-4"
+      label="Full Name"
+      name="name">
+      <UInput v-model="state.fullName" />
+    </UFormGroup>
+
+    <UFormGroup
+      class="mb-4"
+      label="Email"
+      name="email"
+      help="You will receive a confirmation email on both the old and new addresses if you modify the email address.">
+      <UInput v-model="state.email" />
+    </UFormGroup>
+
+    <UButton
+      type="submit"
+      color="black"
+      variant="solid"
+      label="save"
+      :pending="pending" />
+  </UFOrm>
 </template>
+
+<script setup>
+  import { z } from 'zod';
+
+  const supabase = useSupabaseClient();
+  const user = useSupabaseUser();
+
+  const { toastSuccess, toastError } = useAppToast();
+  const pending = ref(false);
+
+  const state = ref({
+    fullName: '',
+    email: user.value?.email,
+  });
+
+  const schema = z.object({
+    fullName: z.string().min(2).optional(),
+    email: z.string().email(),
+  });
+</script>
